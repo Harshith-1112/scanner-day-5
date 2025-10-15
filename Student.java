51af25691075257521ee98a0c0f14e6a64d08ecc
Harshith <harshiharshith665@gmail.com>
2025-10-15 23:22:25 +0530

    Day-3
    Student

---
+++ Student.java
@@ -0,0 +1,36 @@
+public class Student{
+     public static void main(String[] args){
+       int test1=85;
+       int test2=90;
+       int test3=78;
+       // implicit typecasting
+       // calculate average with decimal
+       double average=(test1+test2+test3)/3.0;
+       // explicit typecasting
+       int roundedDownAverage=(int)average;
+       System.out.println("Student Test Scores : "+test1+","+test2+","+test3);
+       System.out.println("Average(Decimal ): "+average);
+       System.out.println("Average(Rounded Down ) "+roundedDownAverage);
+       }
+}
+
+
+
+
+
+
+
+
+
+
+
+       //String studentName="harshith";
+       //int rollNumber=101;
+       //int marks=99;
+       //public static void main(String[] args){
+          //Student s=new Student();
+         // System.out.println("Student Name "+s.studentName);
+         // System.out.println("Student Number "+s.rollNumber);
+          //System.out.println("Student Marks : "+s.marks);
+      // }
+//}
