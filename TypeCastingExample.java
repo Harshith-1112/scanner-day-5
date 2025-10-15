69fb05dbae6a7b202f40532acc3ba619067f3f19
Harshith <harshiharshith665@gmail.com>
2025-10-15 23:25:33 +0530

    Day-4
    TypeCastingExample

---
+++ TypeCastingExample.java
@@ -0,0 +1,14 @@
+public class TypeCastingExample{
+       public static void main(String[] args){
+         int x=10;
+         //int y=20.7f;
+         char ch='a';
+         float f=7;
+         double d=10;
+         System.out.println(x);
+         //System.out.println(y);
+         System.out.println(ch);
+         System.out.println(f);
+         System.out.println(d);
+       }
+}
